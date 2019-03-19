40290e96b055deb38e31e1a06dd7e61d3cd3b897
mochidamochiko <mochidamochiko@users.noreply.github.com>
2019-03-20 04:56:47 +0900

    webhookの認証を追加

--- hello.py
+++ hello.py
@@ -3,7 +3,10 @@
 
 # flaskなどの必要なライブラリをインポート
 import os
-from flask import Flask
+from flask import Flask, request
+
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+VERIFY_TOKEN = os.environ['VERIFY_TOKEN']
 
 # 自分の名称を app という名前でインスタンス化
 app = Flask(__name__)
@@ -11,6 +14,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello World!'
+
+@app.route('/webhook', methods=['GET'])
+def verification():
+
+    if (request.args.get('hub.verify_token', '') == VERIFY_TOKEN):     
+        print("succefully verified")
+        return request.args.get('hub.challenge', '')
+    else:
+        print("Wrong verification token!")
+        return "Wrong validation token"
+
 
 # bashで叩いたかimportで入れたかを判定する
 if __name__ == '__main__':
